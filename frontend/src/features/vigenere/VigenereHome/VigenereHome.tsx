949628909cf0697a4fdfeb7b3401997d3acaecdb
Erkinbekov Bilim <erkinbekov298@gmail.com>
2026-02-20 18:23:55 +0600

    feat(frontend): add VigenereHome page with VigenereForm

---
+++ frontend/src/features/vigenere/VigenereHome/VigenereHome.tsx
@@ -0,0 +1,12 @@
+import Box from '@mui/material/Box';
+import VigenereForm from '../components/VigenereForm/VigenereForm';
+
+const VigenereHome = () => {
+  return (
+    <Box>
+      <VigenereForm />
+    </Box>
+  );
+};
+
+export default VigenereHome;
